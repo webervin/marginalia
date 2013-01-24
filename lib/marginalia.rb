28437b08b29f3acc012789aa4c0a0759758c2253
Ervin Weber <webervin@gmail.com>
2013-01-24 11:39:37 +0200

    Update lib/marginalia.rb
    Added some cases for postgresql_adapter in activerecord 3.2.11

--- lib/marginalia.rb
+++ lib/marginalia.rb
@@ -12,11 +12,31 @@ module Marginalia
           alias_method :execute_without_marginalia, :execute
           alias_method :execute, :execute_with_marginalia
         end
+        if defined? :query
+          alias_method :query_without_marginalia, :query
+          alias_method :query, :query_with_marginalia
+        end
+        if defined? :exec_no_cache
+          alias_method :exec_no_cache_without_marginalia, :exec_no_cache
+          alias_method :exec_no_cache, :exec_no_cache_with_marginalia
+        end
       end
     end
 
     def execute_with_marginalia(sql, name = nil)
-      execute_without_marginalia("#{sql} /*#{Marginalia::Comment.to_s}*/", name)
+      execute_without_marginalia(marginalize_sql(sql), name)
+    end
+
+    def query_with_marginalia(sql, name = nil)
+      query_without_marginalia(marginalize_sql(sql), name = nil)
+    end
+
+    def exec_no_cache_with_marginalia(sql, binds)
+      exec_no_cache_without_marginalia(marginalize_sql(sql), binds)
+    end
+
+    def marginalize_sql(sql)
+      "/*#{Marginalia::Comment.to_s}*/\n#{sql}"
     end
   end
 
